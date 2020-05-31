685e3a20869b15820864a2e2544db3b66c4884c3
DIGDES\Mashutikov.V <Mashutikov.V@digdes.com>
2020-05-31 23:33:31 +0400

    fix: bugs

--- src/main/java/mashutikov/semanticAnalysis/service/NewsFetcher.java
+++ src/main/java/mashutikov/semanticAnalysis/service/NewsFetcher.java
@@ -7,7 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class NewsFetcher {
@@ -21,7 +27,13 @@ public class NewsFetcher {
         List<FeedMessage>  newsList;
         newsList = new ArrayList<>();
         newsList.addAll(apiClient.getNews(SOURCES, RUS_LANG, dto.getQ(), dto.getFrom(), dto.getTo()).getArticles());
-
+        newsList = newsList.stream().filter(distinctByKey(FeedMessage::getUrl)).collect(Collectors.toList());
         return newsList;
+    }
+    public static <T> Predicate<T> distinctByKey(
+            Function<? super T, ?> keyExtractor) {
+
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
 }
